3346bcbfca802653b6aab7b2ec9c14234645367b
Annalakshmi1010 <64143326+Annalakshmi1010@users.noreply.github.com>
2022-03-28 14:15:53 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
 ssh adminuser@40.77.101.19 -o StrictHostKeyChecking=no
-git clone https://github.com/Annalakshmi1010/apache.git
-cd /apache
-sudo su
-mv index.html /var/www/html
+cd ./var/www/html/test.sh
